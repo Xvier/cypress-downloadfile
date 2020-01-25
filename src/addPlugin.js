5b65363ea6959e5b63a2a20bcf3ad4eca29fd03d
Xavier <xviuda@xebia.com>
2020-01-25 14:33:10 +0100

    automatic linting and update package version

--- src/addPlugin.js
+++ src/addPlugin.js
@@ -1,26 +1,32 @@
-const request = require('request');
+const request = require('request')
 const fs = require('fs-extra')
 const path = require('path')
 export function downloadFile(args) {
-
-    const directory = args.directory;
-    const cookieHeader = args.cookies.map(e => e.name + '=' + e.value).join(';');
-    const fileName = args.fileName;
+    const directory = args.directory
+    const cookieHeader = args.cookies.map(e => e.name + '=' + e.value).join(';')
+    const fileName = args.fileName
     return new Promise((resolve, reject) => {
-        request({ url: args.url, encoding: null, headers: { Cookie: cookieHeader } }, function (err, res, body) {
-            if (!res) {
-                return reject(new Error('No response'));
-            }
-            if (res.statusCode !== 200) {
-                return reject(new Error('Bad status code: ' + res.statusCode));
-            }
+        request(
+            {
+                url: args.url,
+                encoding: null,
+                headers: { Cookie: cookieHeader },
+            },
+            function(err, res, body) {
+                if (!res) {
+                    return reject(new Error('No response'))
+                }
+                if (res.statusCode !== 200) {
+                    return reject(
+                        new Error('Bad status code: ' + res.statusCode)
+                    )
+                }
 
+                const file = path.join(directory, fileName)
 
-            
-            const file = path.join(directory , fileName)
-            
-            fs.outputFileSync(file, body)
-            resolve('downloadFile ' + file + ' downloaded');
-        });
-    });
+                fs.outputFileSync(file, body)
+                resolve('downloadFile ' + file + ' downloaded')
+            }
+        )
+    })
 }
