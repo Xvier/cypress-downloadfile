0c7ca06038ff240d1dfd99ce0040716efef3aa50
Aleksandr <63726271+kalcefer@users.noreply.github.com>
2023-01-26 11:08:52 +0300

    Default option

--- src/downloadFileCommand.js
+++ src/downloadFileCommand.js
@@ -1,11 +1,11 @@
-Cypress.Commands.add('downloadFile', (url, dir, fileName, userAgent) => {
-    return cy.getCookies().then(cookies => {
-        return cy.task('downloadFile', {
-            url: url,
-            directory: dir,
-            cookies: cookies,
-            fileName: fileName,
-            userAgent: userAgent,
-        })
-    })
-})
+Cypress.Commands.add('downloadFile', function (url, dir, fileName, userAgent) {
+  return cy.getCookies().then(function (cookies) {
+    return cy.task('downloadFile', {
+      url: url,
+      directory: dir || "cypress/downloads",
+      cookies: cookies,
+      fileName: fileName || url.split("/").pop(),
+      userAgent: userAgent
+    });
+  });
+});
